96639c7b1f326dc7448c5faff7a3fcc66f10c757
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2024-02-06 23:41:41 +0530

    Spring Actuator

---
+++ src/main/java/com/example/aljis/Security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.aljis.Security;
+
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig  {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**").permitAll()
+//                .anyRequest().authenticated())
+//                .formLogin(login -> login.loginPage("/login").permitAll())
+//                .rememberMe(Customizer.withDefaults());
+//        return http.build();
+//    }
+}
